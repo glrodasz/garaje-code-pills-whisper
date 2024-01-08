96252c0f1c94f55186f65a0a9c170ac751374e20
Guillermo Rodas <glrodasz@gmail.com>
2024-01-08 21:04:01 +0100

    transcribe Audio

---
+++ index.js
@@ -0,0 +1,24 @@
+import fs from "node:fs";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+async function transcribeAudio(audioFilePath) {
+  const audioFile = fs.createReadStream(audioFilePath);
+
+  try {
+    const transcript = await openai.audio.transcriptions.create({
+      model: "whisper-1",
+      file: audioFile,
+    });
+
+    return transcript;
+  } catch (error) {
+    console.error("Error trancribing the audio file:", error);
+  }
+}
+
+const transcription = await transcribeAudio("./audio.mp3");
+console.log(transcription);
